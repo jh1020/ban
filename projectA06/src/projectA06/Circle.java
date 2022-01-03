908996f183cd8991eaef881d150debf09f03fc4f
97Kong <ghrn15963@gmail.com>
2022-01-03 19:58:09 +0900

    2

--- projectA06/src/projectA06/Circle.java
+++ projectA06/src/projectA06/Circle.java
@@ -1,5 +1,38 @@
 package projectA06;
 
-public class ProjectA0601 {
+
+
+public class Circle {
+	
+	int radius;
+	String name;
+	
+	public Circle() {
+		
+	}
+	
+	public double gerArea() {
+		return 3.14*radius*radius;
+	}
+	
+	public static void main(String[] args) {
+		
+		Circle pizza;
+		pizza = new Circle();
+		pizza.radius = 10;
+		pizza.name = "자바피자";
+		double area = pizza.gerArea();
+		System.out.println(pizza.name+"의 면적은"+area);
+		
+		
+		Circle donut = new Circle();
+		donut.radius = 2;
+		donut.name = "자바도넛" ;
+		area = donut.gerArea();
+		
+		System.out.println(donut.name+"의 면적은"+area);
+		
+		
+	}
 
 }
